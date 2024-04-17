ffdb910e4396c8e9db2e54184598e16c697243c3
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2024-04-17 22:14:48 +0700

    disable indent blank line

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,7 @@
+return {
+    -- disable indent blank line
+    { "lukas-reineke/indent-blankline.nvim", enabled = false },
+
+    -- disable mini indent scope
+    { "echasnovski/mini.indentscope", enabled = false },
+}
