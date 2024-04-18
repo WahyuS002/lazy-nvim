802ffd54a032125119073f07077e5678101f217f
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2024-04-18 09:58:38 +0700

    feat: disable auto commenting new lines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,8 @@
 
 vim.opt.shiftwidth = 4 -- Size of an indent
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
+
+-- Disable auto commenting new lines
+vim.api.nvim_create_autocmd("FileType", {
+    command = "set formatoptions-=cro",
+})
